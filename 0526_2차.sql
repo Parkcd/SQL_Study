da686bfc08cbcffe9de3e206c8b4e4118ff8b3f1
parksungho <moonhj0105@naver.com>
2022-05-27 21:35:15 +0900

    0527_update

--- 0526_2차.sql
+++ 0526_2차.sql
@@ -32,7 +32,29 @@ SELECT * FROM employees WHERE job_id LIKE 'AD%';
 -- job_id AD이후 3글자
 SELECT * FROM employees WHERE job_id LIKE 'AD___';
 
-SELECT * FROM employees WHERE salary >400 AND job_id = 'IT_PROG'
+SELECT * FROM employees WHERE salary >400 AND job_id = 'IT_PROG';
+
+SELECT 
+UPPER(last_name) UPPER적용 , 
+LOWER(last_name) LOWER적용 , 
+INITCAP(email),
+INITCAP(last_name) 
+FROM employees;  
+
+SELECT job_id , SUBSTR(job_id ,1 ,2) 적용결과 FROM employees;
+
+SELECT job_id , REPLACE(job_id , 'ACCOUNT' , 'ACCNT') 적용결과 FROM employees;
+
+SELECT first_name , LPAD(first_name , 12 , '*') FROM employees;
+
+SELECT 'start'||TRIM('  -space-  ')||'end' 제거된_공백 FROM dual;
+
+SELECT salary , 
+       salary/30 일급,
+       TRUNC(salary/30 ,0) 적용결과0,
+       TRUNC(salary/30 , 1) 적용결과1,
+       TRUNC(salary/30 , -1) 적용결과2
+FROM employees;
 
 
 
